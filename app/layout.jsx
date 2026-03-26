0de75d0ff0e96075a144bd95206e533a61c40eec
dsangermano-spec <dsangermano@livemode.com>
2026-03-26 12:05:10 -0300

    Remove RootLayout component from layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,8 +1,1 @@
-export default function RootLayout({ children }) {
-  return (
-    <html lang="pt-BR">
-      <head><style>{`*{margin:0;padding:0;box-sizing:border-box}body{background:#0f0f0f}`}</style></head>
-      <body>{children}</body>
-    </html>
-  );
-}
+
